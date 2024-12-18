c17c892c09ad3d63b2143111c32826adc52872de
lightknight777 <joseph7777@protonmail.com>
2024-12-17 21:52:02 -0500

    Update server.js
    - Added comments to server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,13 +1,15 @@
-// This is the server file that handles
+// This is the server file. Import middleware using express and cors. Then import movies.route.js for the routes.
 import express from 'express'
 import cors from 'cors'
 import movies from './api/movies.route.js'
 
-const app = express()
+const app = express() 
 
+// Attach cores and express.json middleware that express will use.
 app.use(cors())
 app.use(express.json())
 
+// Specify inital routes for the API
 app.use("api/v1/movies", movies)
 app.use('*', (req, res)=>{
     res.status(404).json({error:"not found"})
