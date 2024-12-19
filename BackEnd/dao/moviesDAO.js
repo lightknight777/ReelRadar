4edd8d5f711d23d57e8daa0c2e82b697a46ea318
lightknight777 <joseph7777@protonmail.com>
2024-12-18 22:07:35 -0500

    Update moviesDAO.js
    - Added code to moviesDAO.js

--- BackEnd/dao/moviesDAO.js
+++ BackEnd/dao/moviesDAO.js
@@ -1,5 +1,16 @@
 let movies
 
 export default class MoviesDAO{
-    
+    static async injectDB(conn) {
+        if(movies){
+            return
+        }
+        try {
+            movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection('movies')
+        }
+        catch(e) {
+            console.error(`unable to connect in MoviesDAO: ${e}`)
+        }
+    }
+
 }
