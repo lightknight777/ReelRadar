7ce7ba681ab1e99c75c23b89227f1b10810ffd48
lightknight777 <joseph7777@protonmail.com>
2024-12-18 23:01:29 -0500

    Update moviesDAO.js
    -Added useful comments to certain blocks of code

--- BackEnd/dao/moviesDAO.js
+++ BackEnd/dao/moviesDAO.js
@@ -15,28 +15,28 @@ export default class MoviesDAO{
         }
     }
 
-    static async getMovies ({//default filter
+    static async getMovies ({//This method accepts a filter as its first argument. The default filter has no filters, retrieves results at page 0 and 20 movies per page.
         filters = null,
         page = 0,
         moviesPerPage = 20, // Will only get 20 movies at once
     } = {}) {
-        let query
+        let query // a query variable which will be empty unless a user specifies filters in the retrieval
         if (filters){
             if ("title" in filters){
-                query = { $text: {$search: filters['title']}}
+                query = { $text: {$search: filters['title']}} // Allows users to query using multiple words.
             } else if ("rated" in filters){
-                query = { "rated": { $eq: filters['rated']}}
+                query = { "rated": { $eq: filters['rated']}} // Checks for the rated filter
             }
         }
 
-        let cursor
+        let cursor // Find all movies that fit our query and assign it to a cursor
         try {
             cursor = await movies
                     .find(query)
                     .limit(moviesPerPage)
                     .skip(moviesPerPage*page)
             const moviesList = await cursor.toArray()
-            const totalNumMovies = await movies.countDocuments(query)
+            const totalNumMovies = await movies.countDocuments(query) // Get the total number of movies by counting the number of documents in the query and return 
             return {moviesList, totalNumMovies}
         }
         catch (e) {
