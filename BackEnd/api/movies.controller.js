19de1b128d523ce83a7de6c7e109c7738c109425
lightknight777 <joseph7777@protonmail.com>
2024-12-19 19:26:49 -0500

    Update movies.controller.js
    - Added additional comments to movies.controller.js

--- BackEnd/api/movies.controller.js
+++ BackEnd/api/movies.controller.js
@@ -5,17 +5,18 @@ export default class MoviesController {
         const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20 // One of the query strings will be movies per page. Check if it exists and parse it into integer
         const page = req.query.page ? parseInt(req.query.page): 0 // The same is done for the page query string
 
-        let filters= {}
+        let filters= {} // Start an empty filters object (no filters applied first)
+
         if (req.query.rated){
-            filters.rated = req.query.rated
+            filters.rated = req.query.rated // Check if the rated query string exists, then add to the filters object.
         }
         else if(req.query.title){
-            filters.title = req.query.title
+            filters.title = req.query.title // Do the same for the title.
         }
 
-        const {moviesList, totalNumMovies} = await MoviesDAO.getMovies({filters, page, moviesPerPage})
+        const {moviesList, totalNumMovies} = await MoviesDAO.getMovies({filters, page, moviesPerPage}) // Call getMovies in MoviesDAO. This will return moviesList and totalNumMovies
 
-        let response = {
+        let response = { // Send a JSON response with the above response object to whoever calls this URL
             movies : moviesList,
             page: page,
             filters : filters,
