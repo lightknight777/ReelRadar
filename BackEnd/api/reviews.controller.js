499c962f891b28a9442f3f0d14a10600abc48122
lightknight777 <joseph7777@protonmail.com>
2024-12-19 23:07:06 -0500

    Update reviews.controller.js
    Added descriptions and comments for the reviews.controller.js

--- BackEnd/api/reviews.controller.js
+++ BackEnd/api/reviews.controller.js
@@ -3,8 +3,8 @@ import ReviewsDAO from '../dao/reviewsDAO.js'
 export default class ReviewsController {
     static async apiPostReview (req, res,next) {
         try {
-            const movieId=req.body.movie_id
-            const review=req.body.review
+            const movieId=req.body.movie_id // Get information from the request's body parameter
+            const review=req.body.review // Data from frontend will be passed in as the request's body
             const userInfo = {
                 name: req.body.name,
                 _id: req.body.user_id
@@ -12,13 +12,13 @@ export default class ReviewsController {
 
             const date = new Date()
 
-            const ReviewResponse = await ReviewsDAO.addReview(
+            const ReviewResponse = await ReviewsDAO.addReview( //Send the information to ReviewsDAO.addReview
                 movieId,
                 userInfo,
                 review,
                 date
             )
-            res.json({status: "success "})
+            res.json({status: "success "}) // Return success if the post works and an error if it didn't
         } catch(e){
             res.status(500).json({error: e.message})
         }
