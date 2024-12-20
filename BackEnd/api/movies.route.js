8051ef8ff6a5b6f41b0990ba2b25f7825f67e410
lightknight777 <joseph7777@protonmail.com>
2024-12-19 19:30:16 -0500

    Update movies.rout.js
    Added import of MoviesController
    Added route to apiGetMovies

--- BackEnd/api/movies.route.js
+++ BackEnd/api/movies.route.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import MoviesController from './movies.controller'
+
 const router = express.Router() // Get access to the express router
-router.route('/').get((req,res)=> res.send('Hello World'))
+router.route('/').get(MoviesController.apiGetMovies)
+
 export default router
