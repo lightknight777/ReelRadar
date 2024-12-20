a3532f9de186a1b3292be4261257640559eb9e8c
lightknight777 <joseph7777@protonmail.com>
2024-12-19 22:16:21 -0500

    Update movies.route.js
    Added routes for Review

--- BackEnd/api/movies.route.js
+++ BackEnd/api/movies.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import MoviesController from './movies.controller.js';
+import ReviewsController from './reviews.controller.js';
 
 const router = express.Router() // Get access to the express router
 router.route('/').get(MoviesController.apiGetMovies);
-//router.route('/:id').get(MoviesController.apiGetMovieById);
+router
+    .route("/review")
+    .post(ReviewsController.apiPostReview)
+    .put(ReviewsController.apiUpdateReview)
+    .delete(ReviewsController.apiDeleteReview)
 
 export default router;
