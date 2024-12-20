d1e9839518fcbf4ca205aa1c72ccd84fc352e752
lightknight777 <joseph7777@protonmail.com>
2024-12-19 19:22:06 -0500

    Update movies.controller.js
    -Added useful comments to the apiGetMovies block

--- BackEnd/api/movies.controller.js
+++ BackEnd/api/movies.controller.js
@@ -1,10 +1,9 @@
 import MoviesDAO from '../dao/moviesDAO.js'
 export default class MoviesController {
 
-    static async apiGetMovies (req, res, next){
-        const moviesPerPage = req.query.moviesPerPage ? 
-    parseInt(req.query.moviesPerPage) : 20
-        const page = req.query.page ? parseInt(req.query.page): 0
+    static async apiGetMovies (req, res, next){ // When apiGetMovies is called via a URL, there will be a query string in the response object
+        const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20 // One of the query strings will be movies per page. Check if it exists and parse it into integer
+        const page = req.query.page ? parseInt(req.query.page): 0 // The same is done for the page query string
 
         let filters= {}
         if (req.query.rated){
